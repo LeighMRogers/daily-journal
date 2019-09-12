2f10f5913014acdfd3f75b590a5d89db61ed99cd
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-09-12 15:33:10 -0500

    got submit button working and things recording to DOM and JSON database

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,10 +1,18 @@
 const api = {
-
-    getJournalEntries: function() {
-        return fetch("http://localhost:3000/journalArray")
+    // fetching the local host - 
+    getJournalEntries: function () {
+        return fetch("http://localhost:3000/journalArray?_sort=date&_order=desc")
             .then(response => response.json());
+    },
+    saveJournalEntries: (entry) => {
+        return fetch("http://localhost:3000/journalArray", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(entry)
+        });
     }
-// fetching the local host - 
 }
 
 export default api
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,43 +1,44 @@
 //Define the keys and value for a JavaScript object that
 //represents a journal entry about what you learned today
 
-//creates the object with properties and values
-// let journalBuilder = (dateValue, conceptValue, entryValue, moodValue) => {
-//     const journalEntry = {
-//         date: dateValue.date,
-//         concept: conceptValue.concept,
-//         entry: entryValue.entry,
-//         mood: moodValue.mood
-//     };
-//     return journalEntry;
-// };
-
-// journalBuilder();
-
 //array - moved to entries.json
 
 import api from "./data.js";
 import injectDOM from "./entriesDOM.js";
 
-function addEntry(newEntry) {
-    journalEntry.push(newEntry);
-}
-
-document.querySelector("#submit--button--id").addEventListener("click", event => 
-    {
-        let dateInput = document.querySelector("#date--id").value;
-        let conceptInput = document.querySelector("concepts--id").value;
-        let entryInput = document.querySelector("journal--entry--id").value;
-        let moodInput = document.querySelector("#mood--entry--id").value;
-
-        makeJournalEntryComponent.innerHTML += journalBuilder(
-            objectBuilder(dateInput, conceptInput, entryInput, moodInput)
-        );
-    });
-
 /*
     Purpose: To render all journal entries to the DOM
     Arguments: entries (array of objects)
 */
-
+//make sure this loads to page
 api.getJournalEntries().then(data => injectDOM.addToDOM(data));
+
+//add event listener to Submit button, grabs the value of the queried IDs and adds it to the DOM.
+const addEntry = document.querySelector("#submit--button--id").addEventListener("click", event => {
+    let dateInput = document.querySelector("#date--id").value;
+    let conceptInput = document.querySelector("#concepts--id").value;
+    let entryInput = document.querySelector("#journal--entry--id").value;
+    let moodInput = document.querySelector("#mood--entry--id").value;
+    console.log(dateInput)
+
+    // creates the object with properties and values
+        const journalEntry = {
+            date: dateInput,
+            concept: conceptInput,
+            entry: entryInput,
+            mood: moodInput
+        };
+    //taking object (journalEntry) and posting to database (api) and saving it there.
+    api.saveJournalEntries(journalEntry).then( () => {
+        //clearing entry log after api pulldown
+        document.querySelector(".entryLog").innerHTML = ""
+        //calling api journal entries because it needs the refreshed data
+        api.getJournalEntries()
+        //take data and add to DOM
+        .then(data => injectDOM.addToDOM(data));
+    })
+        // makeJournalEntryComponent.innerHTML += journalBuilder(
+        //     journalBuilder(dateInput, conceptInput, entryInput, moodInput)
+        // );
+    });
+
